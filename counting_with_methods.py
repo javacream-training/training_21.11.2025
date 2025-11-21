1f371d6a9fcf4ed1f51afc455e7c9966aa60bc81
Pushkar Ranade <pushkar.ranade@aumovio.com>
2025-11-21 14:40:09 +0100

    computer to git

---
+++ counting_with_methods.py
@@ -0,0 +1,39 @@
+recalc = 'Y'
+
+while recalc == 'Y':
+
+    start = input(f'please enter the start number: ')
+    
+    if start.isdigit():
+        
+        end = input(f'please enter the end number: ')
+
+        if end.isdigit():
+            start = int(start)
+            end = int(end)
+
+            if start == end:
+                print(f'cannot count from {start} to {end} if both are equal!')
+            else:
+                if start < end:
+                    print(f'counting upwards from {start} to {end}')
+                    counter = start
+                    while counter <= end:
+                        print(counter)
+                        counter += 1
+                else:
+                    print(f'counting downwards from {start} to {end}')
+                    counter = start
+                    while counter >= end:
+                        print(counter)
+                        counter -= 1
+        else:
+            print(f'Cannot convert end number "{end}" to an integer!')
+
+    else:
+        print(f'Cannot convert start number "{start}" to an integer!')
+            
+    
+    recalc = input('Please enter "Y" to count again: ')
+
+
